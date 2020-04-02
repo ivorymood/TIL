22ae69e36dbc670719840dcdf93e1d4e352ba738
LHY <ivorymood@gmail.com>
2020-04-03 00:08:15 +0900

    20200402_TIL

--- C_300Q/251_/260/Q260.c
+++ C_300Q/251_/260/Q260.c
@@ -6,6 +6,17 @@
 int main(void)
 {
 	char date[] = "2020-06-23";
+	time_t now;
+	struct tm t = { 0, };
+
+	t.tm_mday = atoi(&date[8]);
+	date[7] = 0;
+	t.tm_mon = atoi(&date[5]) - 1;
+	date[4] = 0;
+	t.tm_year = atoi(&date[0]) - 1900;
+
+	now = mktime(&t);
+	printf("2020-06-23을 time_t로 변환하면 %d 입니다. \n", now);
 
 	return 0;
 }
