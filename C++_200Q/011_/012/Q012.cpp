da3b9e0adfea9496e0cd8d6512857f2b120de635
LHY <ivorymood@gmail.com>
2020-04-09 23:56:35 +0900

    20200409_TIL

---
+++ C++_200Q/011_/012/Q012.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// ¡÷ºÆ
+	// cout << "abc" << endl;
+
+	/*
+	for (int i = 0; i < 10; i++)
+	{
+		cout << i << ", ";
+	}
+	*/
+
+	return 0;
+}
