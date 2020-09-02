30d1ebbd54ea81a91d0f3a8ba584b68eecb37859
LHY <ivorymood@gmail.com>
2020-09-03 01:33:35 +0900

    20200908_TIL

---
+++ Python_200Q/001_/Q011.py
@@ -0,0 +1,15 @@
+scope = [1, 2, 3, 4, 5]
+for x in scope:
+    print(x)
+
+str = 'abcdef'
+for c in str:
+    print(c)
+
+ascii_code = {'a': 97, 'b': 98, 'c': 99}
+for c in ascii_code:
+    print(c)
+
+for c in range(10):
+    print(c)
+    
---
+++ Python_200Q/001_/Q015.py
@@ -0,0 +1,10 @@
+val = None
+condition = 2
+if condition == 1:
+    val = [1, 2, 3]
+else:
+    val = 'I love Python'
+
+print(val)
+
+print(None)
