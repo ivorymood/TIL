7fc7e36c579d99b7385df25d46728d51a1f802cd
LHY <ivorymood@gmail.com>
2020-09-03 01:33:35 +0900

    20200902_TIL

---
+++ Python_200Q/001_/Q008.py
@@ -0,0 +1,10 @@
+listdata = ['a', 'b', 'c']
+if 'a' in listdata:
+    print('a가 listdata에 있습니다')
+    print(listdata)
+else :
+    print('a가 listdata에 존재하지 않습니다')
+
+if 'b' in listdata: print('b가 listdata에 존재합니다')
+
+if 'd' not in listdata: print('d가 listdata에 존재하지 않습니다')
